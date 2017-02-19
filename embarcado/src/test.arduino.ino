cb948c3e8b7468e8da43ebb4778f5e3f35c5786f
girlian <girlian_cbj@hotmail.com>
2017-02-19 15:56:59 -0300

    Update test.arduino.ino

--- embarcado/src/test.arduino.ino
+++ embarcado/src/test.arduino.ino
@@ -1,64 +1,72 @@
-#include <ESP8266WiFi.h>
+
+#include <WiFiEsp.h>
+#include <WiFiEspClient.h>
+#include <WiFiEspUdp.h>
+#include "SoftwareSerial.h"
 #include <PubSubClient.h>
 
-#define wifi_ssid "ssid"  // wifi network name
-#define wifi_password "password"  
-
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
-
-#define wifi_ssid "ssid"  // wifi network name
-#define wifi_password "password"  
-
-#define mqtt_server "host/ip" // ip of the machine you are using
+#define mqtt_server "....." //ip of the server
 #define mqtt_user "admin"
 #define mqtt_password "admin"
-
 #define your_topic "UFPA/Belem/Proficional/Itec/Bloco novo/A2/Comunicação/temperatura"
-// variables to the topic
-int valor lido = 0;
-float temp = 0;
-// setting the client 
-WiFiClient espClient;
+#define your_topic2 "UFPA/Belem/Proficional/Itec/Bloco novo/A2/Comunicação/luminosidade"
+// analog input of arduino uno
+const float LM35 = A0;
+const float Ldr = A1;
+float temp;// temperature variable
+float lum; // luminosity variable
+char ssid[] = "....";           // your network SSID (name)
+char pass[] = "....";           // your network password
+int status = WL_IDLE_STATUS;   // the Wifi radio's status
+
+// Initialize the Ethernet client object
+WiFiEspClient espClient;
+
 PubSubClient client(espClient);
 
+SoftwareSerial soft(2,3); // RX, TX
 void setup() {
-  Serial.begin(115200); //initialize the seial of the board used in the work velocity
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);// setting the machine to became the server (machine ip , broker port)
-  
-}
+  // initialize serial for debugging
+  Serial.begin(9600);
+  // initialize serial for ESP module
+  soft.begin(115200);
+  // initialize ESP module
+  WiFi.init(&soft);
 
-void setup_wifi() {
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(wifi_ssid);
-  WiFi.begin(wifi_ssid, wifi_password);
-// in the case of failed conection this function will print points
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
+  // attempt to connect to WiFi network
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(ssid, pass);
+  }
 
+  // you're connected now, so print out the data
+  Serial.println("You're connected to the network");
+
+  //connect to MQTT server
+  client.setServer(mqtt_server, 1883);
+  
+}
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    // If you do not want to use a username and password, change next line to
-    // if (client.connect("ESP8266Client")) {
+    // Attempt to connect, just a name to identify the client
     if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
       Serial.println("connected");
+      
+      
+      
     } else {
-      Serial.print("failed, rc=");
+      Serial.print("failed, reconect=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
@@ -66,25 +74,26 @@ void reconnect() {
     }
   }
 }
-// time counter
+
 long lastMsg = 0;
-int cont = 0;
+
 
 void loop() {
+  // main code here run repeatedly:
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-
   long now = millis();
 
   if (now - lastMsg > 10000) {
     lastMsg = now;
-// pino A0 is the only analogic read , with a 3.2 v max voltage
-    valor Lido = analogRead(A0);
-    temp =  (valor lido * (3,3/1023))*100
-// publish in the form (topic , payload,
-    client.publish(your_topic, String(temp).c_str(), true);
-
+    temp = ((analogRead(LM35))*5/(1023))/0.01;
+    lum = (analogRead(Ldr));
+    client.publish(your_topic, String( temp).c_str(), true);
+    client.publish(your_topic2, String( lum).c_str(), true);
   }
 }
+
+
+ 
